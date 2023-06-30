3dc343a4f2a74cb4e28bf56c9c1e5f969416e37b
Risan Ahamed <risanzahamed76@gmail.com>
2023-06-30 09:52:55 +0600

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./shared/Login"
+import { useEffect } from "react";
 
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
 
   return (
     <>
       <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/*" element={<Home />} />
-    </Routes>
-      
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+
     </>
   )
 }
